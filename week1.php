dd9a63198b9d2f66509628806bfb72f24b645fcc
Hilda Yuliani <hildayuliani.011@gmail.com>
2021-09-15 14:22:08 +0700

    Create week1.php

---
+++ week1.php
@@ -0,0 +1,30 @@
+<?php 
+$nama = "Hilda Yuliani";
+$kelas = "Manajemen Informatika 20 B";
+$alamat = "Dsn. Pamokolan Kec. Cihaurbeuti - Ciamis";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WEEK 1 - Hilda Yuliani</title>
+</head>
+<body>
+    <?php
+        // echo "Hello World";
+        // echo "<br>"; //Untuk memberikan spasi
+        // echo $nama;
+        // echo "<br>";
+        // echo "Hello " .$nama;
+        // echo "<br>";
+        // echo "<br>";
+        echo "BIODATA";
+        echo "<br>"; //Untuk memberikan spasi
+        echo "<br>";
+        echo "Nama : " .$nama;
+        echo "<br>";
+        echo "Kelas : " .$kelas;
+        echo "<br>";
+        echo "Alamat : " .$alamat;
+    ?>
+</body>
+</html>
