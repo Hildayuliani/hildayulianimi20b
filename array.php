7b2c6b1b57b54a5860a48796a13e9a2303d20961
Hildayuliani <90744502+Hildayuliani@users.noreply.github.com>
2021-09-22 10:23:06 +0700

    Add files via upload

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php 
+ 
+//penamaan isi array variabel buah
+$Biodata = array(
+	'Nama' => "Hilda Yuliani",
+	'Jurusan' => "Manajemen Informatika 20 B",
+	'Alamat' => "RT 006 Rw 003 Kp. Cikalapa Ds. Pamokolan Kec. Cihaurbeuti Kab. Ciamis",
+	'No Hp' => "085705935081"
+);
+ 
+// menampilkan isi array yang bernama jeruk
+echo $Biodata['Jurusan'];
+ 
+?>
