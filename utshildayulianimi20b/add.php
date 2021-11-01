8d474f2fe4a20e4fac63b99f5f9a13bc75fd1241
Hilda Yuliani <hildayuliani.011@gmail.com>
2021-11-01 14:29:49 +0700

    update

---
+++ utshildayulianimi20b/add.php
@@ -0,0 +1,120 @@
+<?php
+error_reporting(0);
+//Buat Koneksinya
+$con = new mysqli("localhost","root", "","pasien_hilda");
+$tgl = date ('d F Y');
+
+$query = mysqli_query($con, "SELECT * FROM tabel_pasien");
+/*$result = $con->query($sql);*/
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tambah Pasien</title>
+	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container">
+	<row>
+		<div class="card">
+		<H2 align="center">Tambah Pasien</H2>
+		<div class="card-body">
+			<form class="row g-3" action="add.php" method="post" name="form1">
+			  <div class="col-md-12">
+			    <label for="noPasien" class="form-label">Nomor Pasien</label>
+			    <input type="text" class="form-control" id="noPasien" name="noPasien" placeholder="" required>
+			  </div>
+			  <div class="col-md-12">
+			    <label for="noPasien" class="form-label">Nama Pasien</label>
+			    <input type="text" class="form-control" id="noPasien" name="noPasien" placeholder="" required>
+			  </div>
+			<div class="col-12">
+			    <label for="tglLahir" class="form-label">Tanggal Lahir</label>
+			    <input type="date" class="form-control" id="tglLahir" name="tglLahir" placeholder="mm/dd/yyyy" required>
+			  </div>
+			  <div class="col-12">
+			    <label for="jk" class="form-label">Jenis Kelamin</label><br>
+			    <label><input type="radio" id="jk" name="jk" value="Laki-Laki" required>Laki-Laki</label>
+			    <label><input type="radio" id="jk" name="jk" value="Perempuan" required>Perempuan</label>
+			    <!-- <input type="text" class="form-control" id="jk" name="jk" placeholder="" required> -->
+			  </div>
+			  <div class="col-md-12">
+			    <label for="umr" class="form-label">Umur</label>
+			    <input type="text" class="form-control" id="umr" name="umr" required>
+			  </div>
+			  <div class="col-md-12">
+			    <label for="diagnosa" class="form-label">Diagnosa Penyakit</label>
+			    <input type="text" class="form-control" id="diganosa" name="diagnosa" required>
+			  </div>
+			      <div class="col-md-12">
+			    <label for="namaDokter" class="form-label">Nama Dokter</label>
+			    <input type="text" class="form-control" id="namaDokter" name="namaDokter" required>
+			  </div>
+			  <div class="col-md-12">
+          <label for="jenisPelayanan" class="form-label">Jenis Pelayanan</label>
+          <select id="jenisPelayanan" name="jenisPelayanan" class="form-select" value="<?php echo $isi['jenis_pelayanan']?>" required> 
+            <option selected value="<?php echo $isi['jenis_pelayanan']?>" ><?php echo $js?>Silahkan Pilih..</option>
+            <option value="1">UMUM</option>
+            <option value="2">BPJS</option>
+            <option value="3">JAMKESMAS</option>
+          </select>
+        </div>
+			    <div class="col-12">
+			    <button type="submit" class="btn btn-primary" name="simpan">Add</button>
+			    <button type="button" class="btn btn-danger">Cancel</button>
+			  </div>
+			  </div>
+			</form>
+		</div>
+	</div>
+</row>
+
+
+			      <?php
+  		foreach ($query as $js) {
+    	?>
+    	<option value="<?=$js['jenis_pelayanan']?>"><?=$js['jenis_pelayan']?> </option>
+    	<?php //QUERY DATA TABEL jenis_surat
+		}
+    	?>
+			    </select>
+			 
+			  
+			</form>
+		</div>
+		</div>
+	</row>
+	</div>
+	<?php
+
+
+		if(isset($_POST['simpan'])) {
+			$nomor_pasien = $_POST['nomorPasien'];
+			$nama_pasien = $_POST['namaPasien'];
+			$tgl_lahir = $_POST['tglLahir'];
+			$jenis_kelamin = $_POST['jenisKelamin'];
+			$umur = $_POST['umur'];
+			$diagnosa_penyakit = $_POST['diagnosaPenyakit'];
+			$nama_dokter = $_POST['namaDokter'];
+			$jenis_pelayanan = $_POST['jenisPelayanan'];
+
+			//Insert user data info table
+			$result = mysqli_query($con , "INSERT INTO tabel_pasien
+				(id,nomor_pasien,nama_pasien,tgl_lahir,jenis_kelamin,umur,diagnosa_penyakit,nama_dokter,jenis_pelayanan) 
+				VALUES ('','$nomor_pasien','$nama_pasien','$tgl_lahir','$jenis_kelamin','$umur','$diagnosa_penyakit','$nama_dokter','$jenis_pelayanan')");
+
+			//Show message when user added
+			/*echo "User added Successfully. <a href='view.php'>List Surat</a>";*/
+			header("Location:view.php?pesan=success&&frm=add");
+		}
+	?>
+</body>
+
+<script src="../assets/js/bootstrap.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</html>
---
+++ utshildayulianimi20b/edit.php
@@ -0,0 +1,119 @@
+<?php
+error_reporting(0);
+$con = new mysqli("localhost", "root", "", "pasien_hilda");
+
+$tgl = date('d F Y');
+$id = $_GET['id'];
+$query = mysqli_query($con, "SELECT * FROM tabel_pasien where id = '$id'");
+$isi = $query->fetch_assoc();
+if ($isi['jenis_pelayanan'] == 1) {
+  $js = 'UMUM';
+} else if ($isi['jenis_pelayanan'] == 2) {
+  $js = 'BPJS';
+} else if ($isi['jenis_pelayanan'] == 3) {
+  $js = 'JAMKESMAS';
+} else {
+  $js = 'Jenis Pelayanan Tidak Terdaftar';
+}
+
+?>
+
+<!DOCYTYPE html>
+  <html>
+
+  <head>
+    <title>Update Data Pasien</title>
+    <!-- <link rel="stylesheet" href="../asset/css/bootstrap.min.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </head>
+
+ <body>
+  <div class="container">
+  <row>
+    <div class="card">
+    <H2 align="center">Edit Data Pasien Surat</H2>
+    <div class="card-body">
+      <form class="row g-3" action="edit.php" method="post" name="form1">
+        <input type="hidden" class="form-control" id="id" name="id" value="<?php echo $isi['id']?>" required>
+        <div class="col-md-6">
+          <label for="noPasien" class="form-label">Nomor Pasien</label>
+          <input type="text" class="form-control" id="noPasien" name="noPasien" value="<?php echo $isi['nomor_surat']?>" required>
+        </div>
+        <div class="col-md-6">
+          <label for="jenisPelayanan" class="form-label">Jenis Pelayanan</label>
+          <select id="jenisPelayanan" name="jenisPelayanan" class="form-select" value="<?php echo $isi['jenis_pelayanan']?>" required> 
+            <option selected value="<?php echo $isi['jenis_pelayanan']?>" ><?php echo $js?></option>
+            <option value="1">UMUM</option>
+            <option value="2">BPJS</option>
+            <option value="3">JAMKESMAS</option>
+          </select>
+        </div>
+        <!-- <div class="col-md-6">
+          <label for="inputPassword4" class="form-label">Nomor Surat</label>
+          <input type="password" class="form-control" id="inputPassword4">
+        </div> -->
+        <div class="col-12">
+          <label for="namaPasien" class="form-label">Nama Pasien</label>
+          <input type="text" class="form-control" id="namaPasien" name="namaPasien" placeholder="mm/dd/yyyy" value="<?php echo $isi['nama_pasien']?>" required>
+        </div>
+        <div class="col-12">
+          <label for="tanggalLahir" class="form-label">Tanggal Lahir</label>
+          <input type="date" class="form-control" id="tanggalLahir" name="tanggalLahir" placeholder="" value="<?php echo $isi['tgl_lahir']?>" required>
+        </div>
+        <div class="col-md-6">
+          <label for="umr" class="form-label">Umur</label>
+          <input type="text" class="form-control" id="umr" name="umr" value="<?php echo $isi['umur']?>" required>
+        </div>
+        
+        <div class="col-md-6">
+          <label for="diagnosaPenyakit" class="form-label">Diagnosa Penyakit</label>
+          <input type="text" class="form-control" id="diagnosaPenyakit" name="diagnosaPenyakit" value="<?php echo $isi['diagnosa_penyakit']?>" required>
+        </div>
+        
+        <div class="col-md-6">
+          <label for="namaDokter" class="form-label">Nama Dokter</label>
+          <input type="text" class="form-control" id="namaDokter" name="namaDokter" value="<?php echo $isi['nama_dokter']?>" required>
+        </div>
+
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary" name="update">Update</button>
+          <button type="button" class="btn btn-danger">Batal</button>
+        </div>
+      </form>
+    </div>
+    </div> 
+  </row>
+  </div>
+  <?php
+
+if(isset($_POST['simpan'])) {
+    $nomor_pasien = $_POST['nomorPasien'];
+    $nama_pasien = $_POST['namaPasien'];
+    $tgl_lahir = $_POST['tglLahir'];
+    $jenis_kelamin = $_POST['jenisKelamin'];
+    $umur = $_POST['umur'];
+    $diagnosa_penyakit = $_POST['diagnosaPenyakit'];
+    $nama_dokter = $_POST['namaDokter'];
+    $jenis_pelayanan = $_POST['jenisPelayanan'];
+
+    //Insert user data info table
+    $result = mysqli_query($con , "INSERT INTO tabel_pasien
+        (id,nomor_pasien,nama_pasien,tgl_lahir,jenis_kelamin,umur,diagnosa_penyakit,nama_dokter,jenis_pelayanan) 
+        VALUES ('','$nomor_pasien','$nama_pasien','$tgl_lahir','$jenis_kelamin','$umur','$diagnosa_penyakit','$nama_dokter','$jenis_pelayanan')");
+    
+
+      //Show message when user added
+      /*echo "Surat Updated Successfully. <a href='view.php'>List Surat</a>";*/
+      
+      header("Location:view.php?pesan=success&&frm=edit");
+    }
+  ?>
+  </body>
+  <!-- <script src="../asset/js/bootstrap.min.js"></script> -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  </html>
+
+
